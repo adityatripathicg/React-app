8a8a15f6dbca18656526194deaad57afec25dd45
Aditya Tripathi <adityatripathi3906@gmail.com>
2024-01-30 23:22:33 +0530

    Updated

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import "./App.css";
+import Title from "./Title.jsx";
+import Product from "./Product.jsx";
+import ProductTab from "./ProductTab.jsx";
+
+
+
+function App() {
+  return (
+    <ProductTab/>
+    <ProductTab/>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Description() {
+//   return (
+//     <p> Tony Stark</p>
+//   );
+// }
