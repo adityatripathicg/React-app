f3cf53d1e0910d08bdab1623db2988e011009fef
Aditya Tripathi <adityatripathi3906@gmail.com>
2024-02-17 23:36:08 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Form from "./Form.jsx";
 function App() {
   return (
     <>
-    <h2>Blockbuster Deals | Shop Now</h2>
+    <h2>Blockbuster Deals || Shop Now</h2>
       {/* <ProductTab/> */}
       <Button/>
       <Form/>
